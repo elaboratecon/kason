d48524c0462a030aa5dbd7693707b8c984df508c
elaboratecon <JasonConover@outlook.com>
2024-05-19 12:33:18 -0700

    added remaining 2 pages

---
+++ App/frontend/src/pages/CandidatesForPostings.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+
+import { readFromAPI } from '../helperFunctions'
+
+export const CandidatesForPostings = ({ apiURL }) => {
+    const [candidatesForPostings, setCandidatesForPostings] = useState([])
+
+    function fetchCandidatesForPostings() {
+        readFromAPI(apiURL).then((res) => {
+            setCandidatesForPostings(res)
+        })
+    }
+
+    useEffect(() => {
+        fetchCandidatesForPostings()
+    }, [])
+
+    return (
+        <>
+            <div style={{marginBottom: '1rem'}}>
+                <a href="#" onClick={() => console.log('showAllForms')}>Show All Forms</a>
+            </div>
+
+            <section id="browseCandidatesForPostings">
+            <h2>Browse CandidatesForPostings</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th className="text-center">
+                            <a href="#" onClick={() => console.log('addNewCandidateForPosting')}>New</a>
+                        </th>
+                        <th></th>
+                        <th>candidate_for_posting_id</th>
+                        <th>posting_position</th>
+                        <th>candidate_full_name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        {candidatesForPostings.map((posting, index) => (
+                            <TableRow data={posting} key={index.toString()} />
+                        ))}
+                    </tbody>
+                </table>
+            </section>
+        </>
+    )
+}
+
+const TableRow = ({ data }) => {
+    const {
+        candidate_for_posting_id: candidateForPostingID,
+        posting_position: postingPosition,
+        candidate_full_name: candidateFullName,
+    } = data
+
+    return (
+        <tr>
+            <td>
+                <a href="#" className="text-center" onClick={() => console.log('editCandidateForPosting')}>Edit</a>
+            </td>
+            <td>
+                <a href="#" className="text-center" onClick={() => console.log('deleteCandidateForPosting')}>Delete</a>
+            </td>
+            <td className="text-center">{candidateForPostingID}</td>
+            <td>{postingPosition}</td>
+            <td>{candidateFullName}</td>
+        </tr>
+    )
+}
